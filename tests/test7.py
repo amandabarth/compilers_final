80187c6ee5eab176ca0f30460e71f74a33b7ef4c
amandabarth <112357592+amandabarth@users.noreply.github.com>
2025-05-05 15:01:38 -0400

    fixed test7

--- tests/test7.py
+++ tests/test7.py
@@ -8,9 +8,6 @@ class Point:
     x : int
     y : int
 
-def sum_points(p1: Point, p2: Point) -> int:
-    return ()
-
 s1 = Square(Point(3, 3), Point(1,1), Point(3,1), Point(1,3))
 p1 = s1.a
 print(p1.x)
